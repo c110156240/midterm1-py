8a56d629b76dc217eacaeeac7a06f5fbba2fd7a2
unknown <C110156240@nkust.edu.tw>
2022-04-04 19:44:21 +0800

    22

---
+++ 22.py
@@ -0,0 +1,22 @@
+a=int(input(""))
+for i in range(a):
+    b=input("輸入帳號密碼").split(" ")
+    if b[0]=="123" and b[1]=="456":
+        print("9000")
+    elif b[0]=="456" and b[1]=="789":
+        print("5000")
+    elif b[0]=="789" and b[1]=="888":
+        print("6000")
+    elif b[0]=="336" and b[1]=="558":
+        print("10000")
+    elif b[0]=="566" and b[1]=="221":
+        print("7000")
+    else:
+        print("error")
+
+
+
+
+
+
+        
