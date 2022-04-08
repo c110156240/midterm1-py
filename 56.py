5af4792229bb2e3003f722e25660b8dd7e924c97
unknown <C110156240@nkust.edu.tw>
2022-04-08 19:22:06 +0800

    56

---
+++ 56.py
@@ -0,0 +1,21 @@
+a=int(input("輸入正整數"))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        b=i*j
+        print(b,end="")
+        print(" ",end="")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
